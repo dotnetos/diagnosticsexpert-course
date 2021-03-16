9c4b78df282962ca223e125623fc37e30599f378
Sebastian Solnica <ssolnica@gmail.com>
2021-03-16 13:26:18 +0100

    Module01 exercises

---
+++ Module01-Debugging/requestapp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace requestapp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+
+            var resp = await client.GetAsync("https://asyncexpert.com/");
+
+            Console.WriteLine(await resp.Content.ReadAsStringAsync());
+        }
+    }
+}
