ba13a57415684e3ea34402e0b563bedc504d6a8a
Sebastian Solnica <ssolnica@gmail.com>
2021-03-30 11:16:53 +0200

    Fix for a problem with the CodePage in the first task of the M1 homework (thanks @kapsiR)

--- Module01-Debugging/requestapp/Program.cs
+++ Module01-Debugging/requestapp/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace requestapp
 {
     class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool SetConsoleOutputCP(int codePage);
+
+
         static async Task Main(string[] args)
         {
+            if (!SetConsoleOutputCP(852)) { throw new Win32Exception(); }
+
             using var client = new HttpClient();
 
             var resp = await client.GetAsync("https://asyncexpert.com/");
