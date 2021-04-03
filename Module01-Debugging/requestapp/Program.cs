5d2b0750f18a399cb8ff4edbe7bf894eb700d03c
Sebastian Solnica <ssolnica@gmail.com>
2021-04-03 10:33:35 +0200

    Better way of configuring the 852 codepage
    Thanks to maxfire. Full discussion at https://community.dotnetos.org/t/discussions-module-1-lesson-8-homework/327/25.

--- Module01-Debugging/requestapp/Program.cs
+++ Module01-Debugging/requestapp/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Net.Http;
-using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace requestapp
 {
     class Program
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern bool SetConsoleOutputCP(int codePage);
-
-
         static async Task Main(string[] args)
         {
-            if (!SetConsoleOutputCP(852)) { throw new Win32Exception(); }
+            // based on the recommendation by @maxfire (https://community.dotnetos.org/t/discussions-module-1-lesson-8-homework/327/25)
+            // The code page 852 is required to make the bell ring
+            const int CP = 852;
+
+            // By default, .NET Core does not make available any code page encodings other than code page 28591
+            // and the Unicode encodings, such as UTF-8 and UTF-16. However, you can add the code page encodings
+            // found in standard Windows apps that target .NET to your app.
+            var encoder = CodePagesEncodingProvider.Instance.GetEncoding(CP);
+            Console.OutputEncoding = encoder;
 
             using var client = new HttpClient();
 
